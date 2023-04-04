42c426144caf838968d160edf60ed503ca31eef4
Sergey Rychkov <rich20@mail.ru>
2023-04-05 01:02:42 +0500

    Печать массивов

--- Program.cs
+++ Program.cs
@@ -36,3 +36,15 @@ for (int i = 0; i <= array.Length - 1; i++)
         j++;
     }
 }
+PrintArray(array);
+Console.Write(" -> ");
+PrintArray(newArray);
+void PrintArray(string[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"“{array[i]}”, ");
+    }
+    Console.Write("] ");
+}
